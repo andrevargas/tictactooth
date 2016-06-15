e4c8d52092896f0d67a23b6e735522256c68f4d2
Cristiano_Winter <cristiano.winter@hotmail.com>
2016-06-14 21:11:55 -0300

    Adicionado Biding de botões no mainActivity

--- app/src/main/java/com/programacao/sisnet/tictactooth/MainActivity.java
+++ app/src/main/java/com/programacao/sisnet/tictactooth/MainActivity.java
@@ -2,12 +2,44 @@ package com.programacao.sisnet.tictactooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button a1, a2, a3, b1, b2, b3, c1,c2,c3;
+    Button[] bArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        a1 = (Button) findViewById(R.id.A1);
+        a2 = (Button) findViewById(R.id.A2);
+        a3 = (Button) findViewById(R.id.A3);
+        b1 = (Button) findViewById(R.id.B1);
+        b2 = (Button) findViewById(R.id.B2);
+        b3 = (Button) findViewById(R.id.B3);
+        c1 = (Button) findViewById(R.id.C1);
+        c2 = (Button) findViewById(R.id.C2);
+        c3 = (Button) findViewById(R.id.C3);
+
+        bArray = new Button[]{a1, a2, a3, b1, b2, b3, c1,c2,c3};
+
+        for (Button b : bArray)
+        {
+            b.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        toast("Clicado");
+    }
+
+    private  void toast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
