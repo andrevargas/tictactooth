b23e16e439c1754985108075e815e8d7a4f24790
André Vargas <vargas@geosapiens.com.br>
2016-06-16 18:31:17 -0300

    Adição de botões de controle no layout.

--- app/src/main/java/com/programacao/sisnet/tictactooth/MainActivity.java
+++ app/src/main/java/com/programacao/sisnet/tictactooth/MainActivity.java
@@ -1,15 +1,19 @@
 package com.programacao.sisnet.tictactooth;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button a1, a2, a3, b1, b2, b3, c1,c2,c3;
+    Button a1, a2, a3, b1, b2, b3, c1, c2, c3;
     Button[] bArray;
+    boolean turn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         c2 = (Button) findViewById(R.id.C2);
         c3 = (Button) findViewById(R.id.C3);
 
-        bArray = new Button[]{a1, a2, a3, b1, b2, b3, c1,c2,c3};
+        bArray = new Button[]{a1, a2, a3, b1, b2, b3, c1, c2, c3};
 
         for (Button b : bArray)
         {
@@ -35,8 +39,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
-        toast("Clicado");
+    public void onClick(View v)
+    {
+        Button b = (Button) v;
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "CODE Bold.otf");
+
+        if (turn) {
+            b.setTextSize(60);
+            b.setTextColor(Color.rgb(255, 51, 94));
+            b.setText("X");
+        }
+        else {
+            b.setTextSize(60);
+            b.setTextColor(Color.rgb(22, 151, 215));
+            b.setText("O");
+        }
+
+        b.setTypeface(typeface);
+
+        turn = !turn;
+
+        b.setClickable(false);
+        b.setBackgroundColor(Color.rgb(222, 222, 222));
+        toast("Clicked");
+
     }
 
     private  void toast(String message){
